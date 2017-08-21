93a7b416b07cc7e4bc245deed359eb796a3f9e9e
sonicwater <sonicwater@126.com>
2017-08-21 13:08:21 +0800

    navLeft

--- src/components/NavLeft.vue
+++ src/components/NavLeft.vue
@@ -28,12 +28,12 @@
                         </router-link>
                     </el-menu-item>
                 </el-menu-item-group>
-                <el-menu-item-group title="分组2">
-                    <el-menu-item index="1-3">选项3</el-menu-item>
+                <el-menu-item-group title="绘图">
+                    <el-menu-item index="1-3">原生HTML5 canvas</el-menu-item>
                 </el-menu-item-group>
                 <el-submenu index="1-4">
-                    <template slot="title">选项4</template>
-                    <el-menu-item index="1-4-1">选项1</el-menu-item>
+                    <template slot="title">框架</template>
+                    <el-menu-item index="1-4-1">three.js</el-menu-item>
                 </el-submenu>
             </el-submenu>
             <el-menu-item index="2">
