e54d225a8dadb7b6dac24ee6ebe829ee768f491a
sonicwater <sonicwater@126.com>
2017-08-24 12:29:54 +0800

    fix font

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import elementUi from 'element-ui'
+import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 import VueRouter from 'vue-router'
 import Vuex from 'vuex'
@@ -8,7 +8,7 @@ import Vuex from 'vuex'
 import routerConfig from './routerConfig.js'
 
 
-Vue.use(elementUi);
+Vue.use(ElementUI);
 Vue.use(VueRouter);
 
 const router = new VueRouter(routerConfig);
